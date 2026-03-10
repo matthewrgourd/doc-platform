f7842a61d4f77c3d1b3049b0582dcef9830cfa90
matthewrgourd <mattgourd@googlemail.com>
2026-03-10 15:15:12 +0000

    Fix JSX namespace error for React 19 compatibility

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import {Redirect} from '@docusaurus/router';
 
-export default function Home(): JSX.Element {
+export default function Home(): React.JSX.Element {
   return <Redirect to="/getting-started" />;
 }
